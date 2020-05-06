df84a802e1bd168106f5c26d4dbeea9bbd6a7549
Thiago <thiago.lopes.dev@gmail.com>
2020-05-06 16:06:40 -0300

    if else statements and logical operators

--- src/statements.py
+++ src/statements.py
@@ -5,3 +5,24 @@ if a > b:
     print(a)
 else:
     print(b)
+
+x = 1
+y = 2
+
+if x or y:
+    print('true')
+else:
+    print('false')
+
+x = False
+
+if not x:
+    print('x is true')
+else:
+    print('x is false')
+
+if True + False == 1:
+    print(1)
+if True * True == 1:
+    print(1)
+
