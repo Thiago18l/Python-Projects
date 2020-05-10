40766f9ea4b15155aa74b1d07b88aa5d355ba832
Thiago <thiago.lopes.dev@gmail.com>
2020-05-10 02:03:49 -0300

    add new program to execute with class

---
+++ src/Classes/program.py
@@ -0,0 +1,6 @@
+from src.Classes.student import Student
+
+student1 = Student("Thiago", "ti", 7.0)
+
+print(student1.name)
+print(student1.cre)
---
+++ src/Classes/student.py
@@ -0,0 +1,5 @@
+class Student:
+    def __init__(self, name, major, cre):
+        self.name = name
+        self.major = major
+        self.cre = cre
