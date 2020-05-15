dcdb755f12b0b8eae792e164b81a40f603d66830
Thiago <thiago.lopes.dev@gmail.com>
2020-05-15 00:32:47 -0300

    feat: add new static method a Funcionario

--- src/Advanced/methods.py
+++ src/Advanced/methods.py
@@ -14,6 +14,12 @@ class Funcionario():
     def new_aumento(cls, novo_aumento):
         cls.aumento = novo_aumento
 
+    @staticmethod
+    def dia_util(dia):
+        if dia.weekday() == 5 or dia.weekday() == 6:
+            return 'Não é um dia útil'
+        return 'É um dia útil'
+
 
 thiago = Funcionario("Thiago", 1200)
 print(thiago.dados())
@@ -25,3 +31,8 @@ print(thiago.dados())
 joao = Funcionario("Joao", 5000)
 joao.aplicar_aumento()
 print(joao.dados())
+
+import datetime
+
+minha_data = datetime.date(2020, 5, 16)
+print(thiago.dia_util(minha_data))
